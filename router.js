d8c6d36b5c3e8e4b4b189fc2d7472941c4a7534d
0x5 <239348936+0x5-n0xa@users.noreply.github.com>
2026-06-07 14:51:46 +0900

    Remove Obsolated Repositories

--- router.js
+++ router.js
@@ -1,4 +1,4 @@
-const PAGES        = new Set(['home', 'ttvmin', 'ttvedit', 'noiser', 'dwmrun', 'MemStressKit']);
+const PAGES        = new Set(['home', 'noiser']);
 const DEFAULT_PAGE = 'home';
 const SITE_TITLE   = 'n0xa.f5.si';
 
