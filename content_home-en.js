c89263854f0b2ec26247ca9bc3aba6aca6213f5b
rk0exn <214826087+rk0exn@users.noreply.github.com>
2026-09-14 13:07:53 +0900

    Add local KeepAndroidOpen

--- content_home-en.js
+++ content_home-en.js
@@ -1,5 +1,5 @@
 export default {
-    title: 'My Profile (English)',
+    title: 'My Profile',
     html: /* html */`
 <div class="intro-container">
     <div id="andriod-warn"></div>
@@ -14,7 +14,7 @@ export default {
     <a href="https://github.com/rk0exn" target="_blank">GitHub</a>
     <a href="https://x.com/ik_llmyself" target="_blank">X (formerly Twitter)</a>
     
-    <script src="https://keepandroidopen.org/banner.js?lang=en&id=andriod-warn&size=mini&animation=off&hidebutton=off&link=https://keepandroidopen.org/en/"></script>
+    <script src="https://n0xa.f5.si/KeepAndroidOpen/banner_en_ja.js?lang=en&id=andriod-warn&size=mini&animation=off&hidebutton=off&link=https://keepandroidopen.org/en/"></script>
 </div>
 `,
 };
