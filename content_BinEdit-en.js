b6b73af8c6645a2339a0ceea3cb559f84f4ecaa2
rk0exn <214826087+rk0exn@users.noreply.github.com>
2026-09-17 18:15:36 +0900

    Add No Arrow Style

--- content_BinEdit-en.js
+++ content_BinEdit-en.js
@@ -8,7 +8,7 @@ export default {
         <li>Custom UI powered by Direct3D 11 for fast rendering</li>
         <li>Includes search, bitwise tools, and coloring features</li>
         <li>Supports multiple themes</li>
-        <li>このアプリケーションは<a href="#BinEdit">日本語</a>と英語をサポートしています。</li>
+        <li>このアプリケーションは<a class="no-arrow" href="#BinEdit">日本語</a>と英語をサポートしています。</li>
     </ul>
     <p>We plan to continue expanding its features in the future. Requests are welcome.</p>
     <p>Latest Version: 1.0</p>
